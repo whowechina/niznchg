5d5be66c04d0b4ac5da753243a78bb94623fbf58
Wuhao <whowechina@gmail.com>
2014-12-14 17:08:17 +0800

    Add test points for battery pins. Firmware updates.

--- firmware/niznchg.c
+++ firmware/niznchg.c
@@ -13,79 +13,17 @@ const char fusedata[] __attribute__ ((section (".fuse"))) =
 const char lockbits[] __attribute__ ((section (".lockbits"))) =
 {0xFC};
 
-
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
 
+//#define CHG_FLASH 1   /* Flash while charge, for debug and time adjustment purpose */
 
 typedef unsigned char byte;
 
-#define NTC_3950 /* NTC Type can be 3950£¬3380 or 3435 */
-
-#if defined(NTC_3950)
-const signed char tref[256] PROGMEM =
-{
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 124, 120, 116, 113, 109, 107, 104,
-    101,  99,  97,  95,  93,  91,  90,  88,  86,  85,  84,  82,  81,  80,  78,  77,
-     76,  75,  74,  73,  72,  71,  70,  69,  68,  67,  67,  66,  65,  64,  63,  63,
-     62,  61,  61,  60,  59,  58,  58,  57,  57,  56,  55,  55,  54,  54,  53,  52,
-     52,  51,  51,  50,  50,  49,  49,  48,  48,  47,  47,  46,  46,  45,  45,  44,
-     44,  43,  43,  42,  42,  41,  41,  41,  40,  40,  39,  39,  38,  38,  38,  37,
-     37,  36,  36,  36,  35,  35,  34,  34,  34,  33,  33,  32,  32,  32,  31,  31,
-     31,  30,  30,  29,  29,  29,  28,  28,  28,  27,  27,  27,  26,  26,  26,  25,
-     25,  24,  24,  24,  23,  23,  23,  22,  22,  22,  21,  21,  21,  20,  20,  20,
-     19,  19,  19,  18,  18,  18,  17,  17,  16,  16,  16,  15,  15,  15,  14,  14,
-     14,  13,  13,  13,  12,  12,  12,  11,  11,  11,  10,  10,   9,   9,   9,   8,
-      8,   8,   7,   7,   7,   6,   6,   5,   5,   5,   4,   4,   3,   3,   3,   2,
-      2,   1,   1,   1,   0,   0,  -1,  -1,  -1,  -2,  -2,  -3,  -3,  -4,  -4,  -5,
-     -5,  -6,  -6,  -7,  -7,  -8,  -8,  -9,  -9, -10, -10, -11, -11, -12, -13, -13,
-    -14, -14, -15, -16, -16, -17, -18, -19, -19, -20, -21, -22, -23, -24, -25, -26,
-    -27, -28, -29, -30, -32, -33, -35, -36, -38, -40, -43, -46, -50, -55, -63, -127
-};
-#elif defined(NTC_3380)
-const signed char tref[256] PROGMEM =
-{
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 125, 122, 
-    118, 115, 113, 110, 108, 106, 103, 101,  99,  98,  96,  94,  92,  91,  89,  88,
-     87,  85,  84,  83,  81,  80,  79,  78,  77,  76,  75,  74,  73,  72,  71,  70,
-     69,  68,  67,  67,  66,  65,  64,  63,  63,  62,  61,  60,  60,  59,  58,  58,
-     57,  56,  56,  55,  54,  54,  53,  52,  52,  51,  51,  50,  49,  49,  48,  48,
-     47,  47,  46,  45,  45,  44,  44,  43,  43,  42,  42,  41,  41,  40,  40,  39,
-     39,  38,  38,  37,  37,  37,  36,  36,  35,  35,  34,  34,  33,  33,  32,  32,
-     32,  31,  31,  30,  30,  29,  29,  29,  28,  28,  27,  27,  26,  26,  26,  25,
-     25,  24,  24,  24,  23,  23,  22,  22,  22,  21,  21,  20,  20,  20,  19,  19,
-     18,  18,  18,  17,  17,  16,  16,  16,  15,  15,  14,  14,  14,  13,  13,  12,
-     12,  11,  11,  11,  10,  10,   9,   9,   9,   8,   8,   7,   7,   7,   6,   6,
-      5,  5,    4,   4,   4,   3,   3,   2,   2,   1,   1,   1,   0,   0,  -1,  -1,
-     -2,  -2,  -3,  -3,  -4,  -4,  -5,  -5,  -5,  -6,  -6,  -7,  -7,  -8,  -9,  -9,
-    -10, -10, -11, -11, -12, -12, -13, -14, -14, -15, -15, -16, -17, -17, -18, -19,
-    -19, -20, -21, -21, -22, -23, -24, -25, -25, -26, -27, -28, -29, -30, -31, -32,
-    -34, -35, -36, -38, -39, -41, -42, -44, -46, -49, -51, -55, -59, -64, -73, -127
-};
-#elif defined(NTC_3435)
-const signed char tref[256] PROGMEM =
-{
-    127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 127, 126, 123, 119,
-    116, 114, 111, 108, 106, 104, 102, 100,  98,  96,  94,  93,  91,  90,  88,  87,
-     85,  84,  83,  82,  80,  79,  78,  77,  76,  75,  74,  73,  72,  71,  70,  69,
-     68,  67,  67,  66,  65,  64,  63,  63,  62,  61,  60,  60,  59,  58,  58,  57,
-     56,  56,  55,  54,  54,  53,  52,  52,  51,  51,  50,  50,  49,  48,  48,  47,
-     47,  46,  46,  45,  45,  44,  44,  43,  43,  42,  42,  41,  41,  40,  40,  39,
-     39,  38,  38,  37,  37,  36,  36,  35,  35,  35,  34,  34,  33,  33,  32,  32,
-     31,  31,  31,  30,  30,  29,  29,  28,  28,  28,  27,  27,  26,  26,  26,  25,
-     25,  24,  24,  24,  23,  23,  22,  22,  22,  21,  21,  20,  20,  20,  19,  19,
-     18,  18,  18,  17,  17,  16,  16,  16,  15,  15,  14,  14,  14,  13,  13,  12,
-     12,  12,  11,  11,  10,  10,  10,   9,   9,   8,   8,   8,   7,   7,   6,   6,
-      6,   5,   5,   4,   4,   4,   3,   3,   2,   2,   1,   1,   0,   0,   0,  -1,
-     -1,  -2,  -2,  -3,  -3,  -4,  -4,  -5,  -5,  -6,  -6,  -7,  -7,  -8,  -8,  -9,  
-     -9, -10, -10, -11, -11, -12, -12, -13, -14, -14, -15, -15, -16, -17, -17, -18,
-    -19, -19, -20, -21, -22, -22, -23, -24, -25, -26, -27, -28, -28, -30, -31, -32,
-    -33, -34, -35, -37, -38, -40, -42, -43, -45, -48, -51, -54, -58, -63, -72, -127
-};
-
-#endif
+#define NTC_3380 /* NTC Type can be 3950£¬3380 or 3435 */
+#include "ntc.h"
 
 void init();
 void init_port();
@@ -114,11 +52,13 @@ void batt_alert(char mode);
 #define GREEN   PORTB0
 
 
-#define BATT_VALID_LOW 231
-#define BATT_VALID_HIGH 912
-#define TEMP_VALID_LOW -10
+#define BATT_VALID_LOW 5         /* V / 4.3 / 3.667 * 1023 */
+#define BATT_VALID_HIGH 970      /* V / 4.3 / 3.667 * 1023 */
+#define TEMP_VALID_LOW -30
 #define TEMP_VALID_HIGH 49
 
+#define PHASE_1_DURATION (50 * 60)  /* In seconds */
+#define CHG_TIMEOUT (60 * 60) /* In seconds */
 
 #define CHG_FAST_CURRENT 605    /* I * 0.25 ohm / 4.3 * 20 gain / 3.667v * 1023 */
 
@@ -131,7 +71,7 @@ void batt_alert(char mode);
 #define CHG_PWM_MIN 10
 #define CHG_PWM_MAX 254
 
-#define CHG_FAST_STOP_VOLTAGE 903   /* V / 4.3 / 3.667 * 1023 */
+#define CHG_FAST_STOP_VOLTAGE 900   /* V / 4.3 / 3.667 * 1023 */
 #define CHG_CURRENT_FUSE 1022
 #define CHG_START_THRESHOLD 300  /* detects if charge started */
 #define CHG_CURRENT_TOO_LOW 10   /* detects if battery gone */
@@ -332,7 +272,7 @@ void detect_batt()
     }
 }
 
-#define TICK_PER_SECOND   304           /* Please adjust this number to match the real time */
+#define TICK_PER_SECOND   304     /* This number should be adjusted to match the real time */
 
 signed short charge_fast()
 {
@@ -416,7 +356,7 @@ signed short charge_fast()
         }
         
         /* 1 Hour time out check */
-        if (seconds > 3600) 
+        if (seconds > CHG_TIMEOUT) 
         {
             output_pwm(0);
             return CHG_FULL; /* Finish by Timeout */
@@ -427,42 +367,31 @@ signed short charge_fast()
             old_sec = seconds;
             wdt_reset();
 
-#ifdef DEBUG
-    led_on(GREEN);
-    _delay_ms(1);
-    led_off(GREEN);
+#ifdef CHG_FLASH
+            led_on(GREEN);
+            _delay_ms(1);
+            led_off(GREEN);
 #endif    
 
-            /* Phase two: 50 minutes later */
-            if ((seconds > 3000) && ((seconds & 0x03) == 0)) /* Voltage check every 4 second */
+            /* Phase two */
+            if (seconds > PHASE_1_DURATION) /* Voltage check */
             {
-                /* Voltage measurement, power connected */
-                vbat = (read_adc(VBAT) >> 6) + (read_adc(VBAT) >> 6);
-                vbat = vbat >> 1;
+                /* Disconnect power */
+                output_pwm(0);
                 
-                if (vbat > CHG_FAST_STOP_VOLTAGE)
+                /* Voltage measurement */
+                vbat = (read_adc(VBAT) >> 6) + (read_adc(VBAT) >> 6) + (read_adc(VBAT) >> 6) + (read_adc(VBAT) >> 6);
+                vbat = vbat >> 2;
+
+                /* Resume power */
+                output_pwm(pwm);
+                
+                /* Check if it reaches target voltage */
+                if (vbat >= CHG_FAST_STOP_VOLTAGE)
                 {
-                    /* If the voltage reaches target voltage, do disconnect-and-measure */
                     output_pwm(0);
-#ifdef DEBUG                
-    led_on(GREEN);
-#endif                
-                    _delay_ms(500);
-                    tick += TICK_PER_SECOND / 2;
-#ifdef DEBUG
-    led_off(GREEN);
-#endif                
-                    /* Voltage measurement, power disconnected */
-                    vbat = (read_adc(VBAT) >> 6) + (read_adc(VBAT) >> 6);
-                    vbat = vbat >> 1;
-            
-                    /* Check if it really reaches target voltage */
-                    if (vbat >= CHG_FAST_STOP_VOLTAGE)
-                    {
-                        output_pwm(0);
-                        return CHG_FULL; /* Finish at target voltage */
-                    }
-                }                    
+                    return CHG_FULL; /* Finish at target voltage */
+                }
             }
         }
         
